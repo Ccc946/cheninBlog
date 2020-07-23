7d0e0ca9d187ec0241513b07769c49b3171d2ea3
cheninccc <1185288254@qq.com>
2020-07-23 16:57:29 +0800

    添加静态资源

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+//导入css初始化样式
+import './assets/css/init.css'
+//导入css适配 px2rem
+import 'amfe-flexible'
+//导入动画插件
+import animated from 'animate.css';
+Vue.use(animated)
+
 Vue.config.productionTip = false
 
 new Vue({
